4032920acb0afefb68ac4d1abe121033d67599bb
cioky <cioky@users.noreply.github.com>
2026-06-20 04:26:40 +0300

    v0.4.2: Fix vite.config.ts plugin import, extends config, full parity

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-console.warn(
-  "[vike-ripple] Replace `import vikeRipple from 'vike-ripple'` with `import vikeRipple from 'vike-ripple/config'`",
-)
-export { default } from './config.js'
+export default function vikeRipple() {
+  return {
+    name: 'vike-ripple',
+    enforce: 'pre',
+  }
+}
